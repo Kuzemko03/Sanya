d761203dfda1bfd3d72ba7637f264d2e483cb8af
Алексадр Куземко <kuzemko@sfedu.ru>
2021-10-21 08:53:28 +0300

    Revert "add function SecondsInMinute"
    This reverts commit 09d55c5599ed46121dc2faee692c55fb052cd776.

--- DateTime.pas
+++ DateTime.pas
@@ -4,11 +4,11 @@ begin
   if (y mod 4 = 0) and ((y mod 100 <> 0) or (y mod 400 = 0)) then Result := True;
 end;
 
-function SecondsInMinute(): integer;
+{function SecondsInMinute(): integer;
 begin
   Result := 60;
 end;
-
+}
 begin
   
 end.
