f29a5a1c9c25259b9210e5dc2ef1aeea97d4a1d1
Алексадр Куземко <kuzemko@sfedu.ru>
2021-10-21 08:51:32 +0300

    Add function IsLeapYear

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,14 @@
-﻿begin
+﻿function IsLeapYear(y: integer): boolean;
+begin
+  Result := False;
+  if (y mod 4 = 0) and ((y mod 100 <> 0) or (y mod 400 = 0)) then Result := True;
+end;
+
+function SecondsInMinute(): integer;
+begin
+  Result := 60;
+end;
+
+begin
   
 end.
