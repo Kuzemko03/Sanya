158849445af0373afb7922fdaab959b2c9e0cb9f
Алексадр Куземко <kuzemko@sfedu.ru>
2021-10-21 09:09:14 +0300

    Add LaterInYear

--- DateTime.pas
+++ DateTime.pas
@@ -4,11 +4,30 @@ begin
   if (y mod 4 = 0) and ((y mod 100 <> 0) or (y mod 400 = 0)) then Result := True;
 end;
 
-{function SecondsInMinute(): integer;
+
+function LaterInYear(month1,day1, month2, day2 : integer; var m,d : integer): integer;
 begin
-  Result := 60;
+  if 12- month1 > 12 - month2 then 
+    begin
+      m := month1;
+      d := day1;
+    end;
+  if 12- month1 = 12 - month2 then 
+  begin
+     if 31 - day1 > 31 - day2 then
+     begin
+       m := month1;
+       d := day1;
+     end
+     else
+     begin
+       m := month2;
+       d := day2;
+     end;
+  end;
 end;
-}
+
+
 begin
   
 end.
